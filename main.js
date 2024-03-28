0d6fa0ad6df349f8cbfbac42b2515f8ac3f4bc08
zarnam-izhar <164419157+zarnam-izhar@users.noreply.github.com>
2024-03-29 00:11:36 +0500

    push calculator

---
+++ main.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Select one of the operators to perfom action", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"], },
+]);
+//conditonal statement
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}
+else if (asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}
+else if (asnwer.operator === "Multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}
+else if (asnwer.operator === "Division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+}
